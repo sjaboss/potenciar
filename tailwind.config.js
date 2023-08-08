2a3f723fc1ca0ab207717fd375ea8c4d274c72b1
sjaboss <sjaboss@gmail.com>
2023-08-08 16:41:38 -0300

    109

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/**.vue',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/**.vue",
+    
     ],
+
+    
 
     theme: {
         extend: {
